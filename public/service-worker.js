4cc256a49e09635081d0370f88a3922ef8df33b1
Elijah Romer <romere88@gmail.com>
2021-10-22 09:27:01 -0500

    finished service worker install event handler, and set up scaffolding for activate and fetch event handlers.

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,8 +4,6 @@ const CACHE_NAME = "BT-static-cache-v1";
 const DATA_CACHE_NAME = "BT-data-cache-v1";
 
 const FILES_TO_CACHE = [
-  '/',
-  '/offline.html',
   '/index.html',
   '/index.js',
   '/service-worker.js',
@@ -14,7 +12,25 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png',
 ];
 
-self.addEventListener(`install`, (e) => {
-  console.log(`SERVICE-WORKER INSTALL FIRED`)
+self.addEventListener(`install`, event => {
+  console.log(`SERVICE-WORKER INSTALL EVENT FIRED`);
+  // pre cache all static assets
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+      .catch(err => console.log(err))
+  );
+  // activate immediately once finished installing
+  self.skipWaiting();
+})
+
+self.addEventListener(`activate`, event => {
+  console.log(`SERVICE-WORKER ACTIVATE EVENT FIRED`)
+})
+
+self.addEventListener(`fetch`, event => {
+  console.log(`SERVICE-WORKER FETCH EVENT FIRED`)
+  console.log(event)
+  console.log(event.request)
 })
 
