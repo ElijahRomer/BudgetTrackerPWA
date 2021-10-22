19ed0e17ac816c839e675e3c380ee25aeaaa534e
Elijah Romer <romere88@gmail.com>
2021-10-22 09:18:11 -0500

    added CACHE_NAME, DATA_CACHE_NAME, and FILES_TO_CACHE to the service-worker. Began set up of install event handler.

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,20 @@
 console.log(`Hello From your Service Worker!`)
+
+const CACHE_NAME = "BT-static-cache-v1";
+const DATA_CACHE_NAME = "BT-data-cache-v1";
+
+const FILES_TO_CACHE = [
+  '/',
+  '/offline.html',
+  '/index.html',
+  '/index.js',
+  '/service-worker.js',
+  '/styles.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+];
+
+self.addEventListener(`install`, (e) => {
+  console.log(`SERVICE-WORKER INSTALL FIRED`)
+})
+
